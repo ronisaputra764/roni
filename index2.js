cb7268cbbe3374721bc2413c1571dae52b0d7638
ronisaputra764 <157409489+ronisaputra764@users.noreply.github.com>
2024-02-15 20:07:41 +0700

    index2.js

---
+++ index2.js
@@ -0,0 +1,24 @@
+//mengubah tipe data
+
+// to integer
+var intResult = parseInt(a)
+console.log(typeof(intResult))
+intResult = parseInt(b)
+console.log(intResult)
+
+// to float
+var fltResult = parseFloat(a)
+console.log(fltResult)
+fltResult = parseFloat(b)
+console.log(fltResult)
+
+// numbers
+var c = 3
+var d = 3.14
+
+// to string
+var strResult = c.toString()
+console.log(strResult)
+// from float to string
+strResult = d.toString()
+console.log(strResult)
